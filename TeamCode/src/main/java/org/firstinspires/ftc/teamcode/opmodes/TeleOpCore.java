5d3a80f7f2011c589ecf21e110f89147a728dbb4
Caroline Bethel <92123058+cvbethel@users.noreply.github.com>
2021-10-23 10:02:11 -0400

    Thin bot config and tele op

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOpCore.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOpCore.java
@@ -1,26 +1,51 @@
-package org.firstinspires.ftc.teamcode.testopmodes.drivetests;
+package org.firstinspires.ftc.teamcode.opmodes;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
+@Config
 @TeleOp
-public class ThinBotTest extends OpMode {
+public class TeleOpCore extends OpMode {
+    Servo outServo;
+    DcMotor intakeMotor;
     DcMotor frontRightMotor;
     DcMotor frontLeftMotor;
     DcMotor backRightMotor;
     DcMotor backLeftMotor;
 
+    public static double IN = 1;
+    public static double OUT = 0;
+
     @Override
     public void init(){
+        outServo = hardwareMap.get(Servo.class, "outServo");
+        intakeMotor = hardwareMap.get(DcMotor.class, "intakeMotor");
         frontRightMotor = hardwareMap.get(DcMotor.class, "frontRightMotor");
         frontLeftMotor = hardwareMap.get(DcMotor.class, "frontLeftMotor");
         backRightMotor = hardwareMap.get(DcMotor.class, "backRightMotor");
         backLeftMotor = hardwareMap.get(DcMotor.class, "backLeftMotor");
+
     }
 
     @Override
     public void loop(){
+        if(gamepad2.a){
+            outServo.setPosition(IN);
+        }
+
+        if(gamepad2.b){
+            outServo.setPosition(OUT);
+        }
+
+        if(gamepad2.left_trigger > 0.05){
+            intakeMotor.setPower(-gamepad2.left_trigger);
+        } else if(gamepad2.right_trigger > 0.05){
+            intakeMotor.setPower(gamepad2.right_trigger);
+        }
+
         double vertical;
         double horizontal;
         double pivot;        double speed;
