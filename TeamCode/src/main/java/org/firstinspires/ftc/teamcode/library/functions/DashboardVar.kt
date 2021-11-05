f9a37cd7ad43afa605c5384f5aeb69be881ac34d
Steven Baker <42903115+sbdevelops@users.noreply.github.com>
2021-11-05 12:58:33 -0400

    Added DashboardVarWrapped for TelemetryMenu compatibility

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/library/functions/DashboardVar.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/library/functions/DashboardVar.kt
@@ -20,13 +20,8 @@ class DashboardVar<T>(
             clazz.simpleName,
             name,
             object: ValueProvider<T> {
-                override fun get(): T {
-                    return storedValue
-                }
-
-                override fun set(value: T) {
-                    if (validator?.invoke(value) != false) storedValue = value
-                }
+                override fun get(): T { return storedValue }
+                override fun set(value: T) { if (validator?.invoke(value) != false) storedValue = value }
             },
             true
         )
@@ -43,4 +38,36 @@ class DashboardVar<T>(
                 "within property ${property.name}")
     }
 
+}
+
+class DashboardVarWrapped<T>(
+    property: ReadWriteProperty<Any?, T>,
+    private val name: String,
+    private val clazz: KClass<*>
+): ReadWriteProperty<Any?, T> {
+
+    var storedValue: T by property
+
+    init {
+        FtcDashboard.getInstance().addConfigVariable(
+            clazz.simpleName,
+            name,
+            object: ValueProvider<T> {
+                override fun get(): T { return storedValue }
+                override fun set(value: T) { storedValue = value }
+            },
+            true
+        )
+    }
+
+    override fun getValue(thisRef: Any?, property: KProperty<*>): T {
+        return storedValue
+    }
+
+    override fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+        storedValue = value
+        println("DashboardVar received value update for " +
+                "${clazz.qualifiedName}:${clazz.simpleName}.${name} " +
+                "within property ${property.name}")
+    }
 }
