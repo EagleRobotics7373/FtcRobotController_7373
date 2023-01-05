6375d380f44782bd8562a30e45f7110b3ae18ab1
Sam Haygood <57695522+samhaygood05@users.noreply.github.com>
2023-01-05 16:03:56 -0500

    Implemented new claw system into TeleOP

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/library/robot/robotcore/ExtThinBot.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/library/robot/robotcore/ExtThinBot.kt
@@ -5,17 +5,18 @@ import com.qualcomm.robotcore.hardware.HardwareMap
 import com.qualcomm.robotcore.hardware.Servo
 import org.firstinspires.ftc.teamcode.library.robot.systems.drive.roadrunner.HolonomicRR
 import org.firstinspires.ftc.teamcode.library.robot.systems.drive.roadrunner.TwoWheelOdometryLocalizer
+import org.firstinspires.ftc.teamcode.library.robot.systems.lt.DualServoClawLift
 import org.firstinspires.ftc.teamcode.library.robot.systems.meet1.LiftClawSystem
 
 class ExtThinBot(_hardwareMap: HardwareMap): BaseRobot(_hardwareMap) {
 
     @JvmField val linearActuatorMotor : DcMotorEx = hwInit("linearActuatorMotor")
-    @JvmField val liftMotor : DcMotorEx = hwInit("liftMotor")
-    @JvmField val clawServo : Servo = hwInit("clawServo")
+    @JvmField val leftClawServo : Servo = hwInit("leftClawServo")
+    @JvmField val rightClawServo : Servo = hwInit("rightClawServo")
     @JvmField val odometryRight : DcMotorEx = hwInit("odometryRight")
     @JvmField val odometryCenter : DcMotorEx = hwInit("odometryCenter")
 
-    @JvmField val liftClawSystem = LiftClawSystem(linearActuatorMotor, liftMotor, clawServo)
+    @JvmField val dualServoClawLift = DualServoClawLift(linearActuatorMotor, leftClawServo, rightClawServo)
 
     @JvmField val imuControllerC = IMUController(hardwareMap, id = 'C')
     override val holonomicRR: HolonomicRR = HolonomicRR(imuControllerC, frontLeftMotor, backLeftMotor, backRightMotor, frontRightMotor,
